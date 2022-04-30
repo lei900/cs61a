84f3c6aac8d5692fc7fec56719eea8efa7da39be
lei <mia.wang90@gmail.com>
2022-04-30 23:31:53 +0900

    parsons probs1

---
+++ lab_22/lab02/parsons_probs/hop.py
@@ -0,0 +1,16 @@
+def hop():
+    """
+    Calling hop returns a curried version of the function f(x, y) = y.
+    >>> hop()(3)(2) # .Case 1
+    2
+    >>> hop()(3)(7) # .Case 2
+    7
+    >>> hop()(4)(7) # .Case 3
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    def f(x):
+        def g(y):
+            return y
+        return g
+    return f
