b85ad5b7afcc6ce407c8bd4b539d1dcd1a2f40c3
lei <mia.wang90@gmail.com>
2022-05-02 11:54:45 +0900

    disc01-q8

---
+++ disc/disc02/q8.py
@@ -0,0 +1,39 @@
+from turtle import right
+
+
+def match_k(k):
+    """ Return a function that checks if digits k apart match
+
+    >>> match_k(2)(1010)
+    True
+    >>> match_k(2)(2010)
+    False
+    >>> match_k(1)(1010)
+    False
+    >>> match_k(1)(1)
+    True
+    >>> match_k(1)(2111111111111111)
+    False
+    >>> match_k(3)(123123)
+    True
+    >>> match_k(2)(123123)
+    False
+    """
+    # def check(x):
+    #     while x // (10**k) > 0:
+    #         if x % 10 == (x // (10**k)) % 10:
+    #             x = x // 10
+    #         else:
+    #             return False
+    #     return True
+            
+    # return check
+
+    # solution
+    def check(x):
+        while x // (10**k):
+            if (x % 10) != (x // (10**k)) % 10:
+                return False
+            x = x // 10
+        return True
+    return check
