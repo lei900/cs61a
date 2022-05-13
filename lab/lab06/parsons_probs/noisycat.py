cbb0d23c7e8bc5f11463e8118471ebc6869053e1
lei <mia.wang90@gmail.com>
2022-05-13 12:03:03 +0900

    lab06 finished

---
+++ lab/lab06/parsons_probs/noisycat.py
@@ -0,0 +1,44 @@
+class Cat:
+    def __init__(self, name, owner, lives=9):
+        self.is_alive = True
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+
+    def talk(self):
+        return self.name + ' says meow!'
+
+
+class NoisyCat(Cat):
+    """
+    >>> my_cat = NoisyCat("Furball", "James")
+    >>> my_cat.name
+    'Furball'
+    >>> my_cat.is_alive
+    True
+    >>> my_cat.lives
+    8
+    >>> my_cat.talk()
+    'Furball says meow! Furball says meow!'
+    >>> friend_cat = NoisyCat("Tabby", "James", 2)
+    >>> friend_cat.talk()
+    'Tabby says meow! Tabby says meow!'
+    >>> friend_cat.lives
+    1
+    """
+
+    def __init__(self, name, owner, lives=9):
+        "*** YOUR CODE HERE ***"
+        # self.is_alive = True
+        # self.name = name
+        # self.owner = owner
+        super().__init__(name, owner, lives)
+        self.lives -= 1
+
+    def talk(self):
+        "*** YOUR CODE HERE ***"
+        # return self.name + " says meow!" + " " + self.name + " says meow!"
+        words = super().talk()
+        words = words + " " + words
+        return words
+
