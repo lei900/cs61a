7e383b9c0c919d8f187220a25350a74bcf0b4caf
lei <mia.wang90@gmail.com>
2022-05-18 11:20:32 +0900

    scheme lab finished

---
+++ lab/lab10/lab10.scm
@@ -0,0 +1,23 @@
+(define (over-or-under num1 num2) 
+    (cond 
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1)))
+
+(define (make-adder num) 
+    (lambda (inc) (+ inc num)
+    )
+)
+
+(define (composed f g) 
+    (lambda (x) (f (g x))
+    )
+)
+
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (cond 
+    ((= exp 0)   1)
+    ((even? exp) (square (pow base (/ exp 2))))
+    (else        (* base (pow base (- exp 1))))))
