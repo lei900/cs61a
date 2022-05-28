69878cf8c3e29ee8498c2954abaf44242a0b5600
lei <mia.wang90@gmail.com>
2022-05-28 19:18:05 +0900

    problem 6

--- ants/ants.py
+++ ants/ants.py
@@ -279,7 +279,14 @@ class FireAnt(Ant):
         # END Problem 5
 
 # BEGIN Problem 6
-# The WallAnt class
+class WallAnt(Ant):
+    name = 'Wall'
+    food_cost = 4
+    implemented = True 
+
+    def __init__(self, health=4):
+        """Create an Ant with a HEALTH quantity."""
+        super().__init__(health)
 # END Problem 6
 
 # BEGIN Problem 7
